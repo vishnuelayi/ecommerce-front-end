6361767c9c4dc330233f982090fc4a62f101d525
Vishnu Elayi <vishnuelayi@gmail.com>
2024-01-02 01:20:25 +0530

    completed ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 function ProductCard() {
   return (
-    <div className='col-3'>
-    <div className='product-card'>
-      <div className='product-image'>
-      <img src='images/watch.jpg' className='img-fluid' alt='product-img'/>
+    <div className="col-3">
+      <div className="product-card position-relative">
+      <div className="wishlist-icon position-absolute">
+      <img src="images/wish.svg" alt="wishlist-icon" />
+
       </div>
-      <div className='product-card-details'>
-        <h6 className='brand'>Rolex</h6>
-        <h5 className='product-title'>Carribien Wonder</h5>
-      
-        <p className='price'>₹1,39990</p>
+        <div className="product-image">
+          <img src="images/watch.jpg" className="img-fluid" alt="product-img" />
+        </div>
+        <div className="product-card-details">
+          <h6 className="brand">Rolex</h6>
+          <h5 className="product-title">Carribien Wonder</h5>
+
+          <p className="price">₹1,39990</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/view.svg" alt="cart-icon" />
+            </Link>
+            <Link>
+              <img src="images/prodcompare.svg" alt="heart-icon" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="cart-icon" />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
-      
-    </div>
-  )
+  );
 }
 
-export default ProductCard
+export default ProductCard;
