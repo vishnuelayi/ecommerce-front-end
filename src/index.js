fb81b6442c9207ba8f5bb1497b85f22fbfe3ae1c
Vishnu Elayi <vishnuelayi@gmail.com>
2024-05-12 14:10:10 +0530

    ready to deploy front-end

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import { store } from "./app/store";
 import App from "./App";
 import { Provider } from "react-redux";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
